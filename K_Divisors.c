75de50bb5b9445f8cb1f9648d34687c3a2ec6c23
Nasim <mdnasimhosen333@gmail.com>
2025-04-15 09:28:53 +0600

    extra problem 2 solved

--- K_Divisors.c
+++ K_Divisors.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
-        if(n % i == 0){
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             printf("%d\n", i);
         }
     }
