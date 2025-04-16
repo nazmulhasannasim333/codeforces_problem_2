b43ba0ebc10e02406eab5e001433cda9cfd0d9a6
Nasim <mdnasimhosen333@gmail.com>
2025-04-16 12:52:07 +0600

    sequence number sum

---
+++ R_Sequence_of_Numbers_and_Sum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, M;
+    for (;;)
+    {
+        scanf("%d %d", &N, &M);
+
+        if (N <= 0 || M <= 0)
+        {
+            break;
+        }
+
+        if (N > M)
+        {
+            int temp = N;
+            N = M;
+            M = temp;
+        }
+        int sum = 0;
+        for (int i = N; i <= M; i++)
+        {
+            printf("%d ", i);
+            sum += i;
+        }
+        printf("sum =%d\n", sum);
+    }
+    return 0;
+}
