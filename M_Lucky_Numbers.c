9f784cafe148c0b304f067b5124320a729ba1291
Nasim <mdnasimhosen333@gmail.com>
2025-04-26 21:13:38 +0600

    Fix output formatting and add problem metadata for "M. Lucky Numbers"

--- M_Lucky_Numbers.c
+++ M_Lucky_Numbers.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
-// Fail test case, need to resolve the problem - wrong output
 int main()
 {
     int A, B;
     scanf("%d %d", &A, &B);
+
     int found = 0;
 
     for (int i = A; i <= B; i++)
     {
-        int num = i;
         int isLucky = 1;
-        while (num > 0)
+        int number = i;
+
+        while (number > 0)
         {
-            int digit = num % 10;
+            int digit = number % 10;
             if (digit != 4 && digit != 7)
             {
                 isLucky = 0;
                 break;
             }
-            num /= 10;
+            number /= 10;
         }
+
         if (isLucky)
         {
-            printf("%d ", i);
+            if (found)
+            {
+                printf(" ");
+            }
+            printf("%d", i);
             found = 1;
         }
     }
@@ -33,5 +39,6 @@ int main()
         printf("-1");
     }
 
+    printf("\n");
     return 0;
 }
